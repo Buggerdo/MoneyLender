dbc89f790373a0e54d6a9bf097fcdb0b10682a83
Troy <wilson.troy.anthony@gmail.com>
2022-07-15 20:32:18 -0400

    stuff

--- MoneyLender/Program.cs
+++ MoneyLender/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Guy Joe = new Guy() { Cash = 50, Name = "Joe" };
+            Guy Bob = new Guy() { Cash = 100, Name = "Bob" };
+
+            while(true)
+            {
+                Joe.WriteMyInfo();
+                Bob.WriteMyInfo();
+                Console.Write("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+                if(howMuch == "") return;
+                if(int.TryParse(howMuch, out int IntHowMuch))
+                {
+                    Console.Write("Who should give the cash: ");
+                    string whichGuy = Console.ReadLine();
+                    if(whichGuy == Joe.Name)
+                    {
+                        int give = Joe.GiveCash(IntHowMuch);
+                        Bob.ReceiveCash(give);
+                    }
+                    else if(whichGuy == Bob.Name)
+                    {
+                        int give = Bob.GiveCash(IntHowMuch);
+                        Joe.ReceiveCash(give);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter Joe or Bob");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or a blank line to exit).");
+                }
+            }
         }
     }
 }
